52ce0626badd652c2e501126570e657a67fa9b32
P4OX <John213822@gmail.com>
2025-02-05 06:43:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -35,58 +35,6 @@ document.getElementById("nukeButton").addEventListener("click", function() {
 
 document.getElementById("deleteWebhookButton").addEventListener("click", function() {
     const webhookUrl = document.getElementById("webhookUrl").value;
+    const botToken = document.getElementById("botToken").value;
 
-    if (isValidUrl(webhookUrl)) {
-        document.getElementById("message").textContent = `Deleting webhook...`;
-        document.getElementById("message").style.color = "#FF4D4D";
-
-        deleteWebhook(webhookUrl);
-    } else {
-        document.getElementById("message").textContent = "Invalid webhook URL!";
-        document.getElementById("message").style.color = "#FF5733";
-    }
-});
-
-function isValidUrl(url) {
-    const pattern = new RegExp('https://discord.com/api/webhooks/([A-Za-z0-9_-]+)/([A-Za-z0-9_-]+)');
-    return pattern.test(url);
-}
-
-function sendWebhook(url, pingRole, customMessage, count) {
-    let content = customMessage ? customMessage : `Nuke message #${count}`;
-    
-    if (pingRole) {
-        content = `<@${pingRole}> ${content}`;
-    }
-
-    const data = { 
-        content: content,
-        username: "Axiom Spammer Bot",
-    };
-
-    fetch(url, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data),
-    })
-    .then(response => {
-        if (response.ok) {
-            document.getElementById("message").textContent = `Webhook #${count} sent successfully!`;
-            document.getElementById("message").style.color = "#00FF00";
-        } else {
-            document.getElementById("message").textContent = `Failed to send webhook #${count}`;
-            document.getElementById("message").style.color = "#FF5733";
-        }
-    })
-    .catch(error => {
-        document.getElementById("message").textContent = `Error: ${error}`;
-        document.getElementById("message").style.color = "#FF5733";
-    });
-}
-
-function deleteWebhook(url) {
-    setTimeout(() => {
-        document.getElementById("message").textContent = "Webhook deleted successfully!";
-        document.getElementById("message").style.color = "#FF4D4D";
-    }, 1000);
-}
+    if (isValidUrl(webhookUrl) && botToken) {
